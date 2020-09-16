ad77f1084ecca585f4ab0ab5830f8d800ee856d4
JonathanGzzBen <jonathangzzben@gmail.com>
2020-09-16 14:29:39 -0500

    Add YoutubeDownloader class

--- YoutubeDownloader.Core/YoutubeDownloader.cs
+++ YoutubeDownloader.Core/YoutubeDownloader.cs
@@ -1,10 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.IO;
+using System.Threading.Tasks;
+using YoutubeExplode;
+using YoutubeExplode.Videos;
+using YoutubeExplode.Videos.Streams;
 
 namespace YoutubeDownloader.Core
 {
-    class YoutubeDownloader
+    public class YoutubeDownloader
     {
+        public static async Task<Video> GetVideo(string url)
+        {
+            var youtube = new YoutubeClient();
+            return await youtube.Videos.GetAsync(url);
+        }
+
+        public static async Task<Stream> GetMuxedWithHighestVideoQualityStream(string url)
+        {
+            var youtube = new YoutubeClient();
+            var streamManifest = await youtube.Videos.Streams.GetManifestAsync(url);
+            var streamInfo = streamManifest.GetMuxed().WithHighestVideoQuality();
+            if (streamInfo != null)
+            {
+                return await youtube.Videos.Streams.GetAsync(streamInfo);
+            }
+
+            return null;
+        }
     }
 }
