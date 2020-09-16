039f4ed46a068666b3ed399d780ba809df27e1c8
JonathanGzzBen <jonathangzzben@gmail.com>
2020-09-16 14:30:10 -0500

    Add Tests project and YoutubeDownloader tests

---
+++ YoutubeDownloader.Tests/YoutubeDownloaderSpecs.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace YoutubeDownloader.Tests
+{
+    public class YoutubeDownloaderSpecs
+    {
+        [Theory]
+        [InlineData("6RWfxcXCDrc")]
+        [InlineData("https://www.youtube.com/watch?v=6RWfxcXCDrc")]
+        public async Task GetVideo_ValidUrl_NotNullObject(string url)
+        {
+            var video = await Core.YoutubeDownloader.GetVideo(url);
+            Assert.NotNull(video);
+        }
+
+        [Theory]
+        [InlineData("6RWfxcXCDrc")]
+        [InlineData("https://www.youtube.com/watch?v=XpS2i6ESnjE")]
+        public async Task GetMuxedTiwhHighestVideoQualityStream_ValidUrl_GetsValidVideoStream(string url)
+        {
+            await using var stream = await Core.YoutubeDownloader.GetMuxedWithHighestVideoQualityStream(url);
+            var testFileName = @"test.mp4";
+            await using var fileStream = File.Create(testFileName);
+            await stream.CopyToAsync(fileStream);
+
+            Assert.True(File.Exists(testFileName));
+
+            File.Delete(testFileName);
+        }
+    }
+}
