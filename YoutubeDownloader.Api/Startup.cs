70d074ea921c2017f469578e61af60e79c2aecd0
JonathanGzzBen <jonathangzzben@gmail.com>
2020-09-16 14:52:28 -0500

    Add Swagger UI

--- YoutubeDownloader.Api/Startup.cs
+++ YoutubeDownloader.Api/Startup.cs
@@ -1,15 +1,9 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace YoutubeDownloader
 {
@@ -26,6 +20,8 @@ namespace YoutubeDownloader
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,6 +33,14 @@ namespace YoutubeDownloader
             }
 
             app.UseHttpsRedirection();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "YoutubeDownloader API V1");
+                c.RoutePrefix = String.Empty;
+            });
 
             app.UseRouting();
 
